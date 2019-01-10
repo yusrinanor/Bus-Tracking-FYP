5a80b3d777f24cd51fc5e4cc99bd21b2ee32292f
yusrina nor <yusrina.nor23@live.com>
2019-01-11 01:28:36 +0800

    SCP Report

--- app/src/main/java/com/example/android/fyp/VistaReport.java
+++ app/src/main/java/com/example/android/fyp/VistaReport.java
@@ -4,13 +4,23 @@ package com.example.android.fyp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+
 public class VistaReport extends Fragment {
 
     private View view;
+    private RecyclerView reportRecyclerView;
+    private LinearLayoutManager layoutManager;
+    private FirebaseRecyclerAdapter<Report, ReportHolder> reportAdapter;
+    private DatabaseReference mDatabaseReference;
+    private DatabaseReference reportRef;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
