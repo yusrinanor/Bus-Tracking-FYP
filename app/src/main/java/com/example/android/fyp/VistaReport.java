ba95ea4a741b15d598ae213825576d1f0ec87877
yusrina nor <yusrina.nor23@live.com>
2019-01-11 01:31:28 +0800

    Vista report

--- app/src/main/java/com/example/android/fyp/VistaReport.java
+++ app/src/main/java/com/example/android/fyp/VistaReport.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class VistaReport extends Fragment {
 
@@ -25,6 +26,25 @@ public class VistaReport extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.vistareport, container, false);
+        layoutManager = new LinearLayoutManager(getActivity());
+        reportRecyclerView = (RecyclerView) view.findViewById(R.id.reportRecycler);
+        reportRecyclerView.setHasFixedSize(true);
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        reportRef = mDatabaseReference.child("Report");
+        reportAdapter = new FirebaseRecyclerAdapter<Report, ReportHolder>(Report.class, R.layout.cardviewreport, ReportHolder.class,  reportRef.orderByChild("route").equalTo("VISTA-APU")) {
+            @Override
+            protected void populateViewHolder(ReportHolder viewHolder, Report model, int position) {
+                viewHolder.txtDate.setText(viewHolder.txtDate.getText().toString() + ":" + model.getDate());
+                viewHolder.txtFrom.setText(viewHolder.txtFrom.getText().toString() + ":" + model.getFrom());
+                viewHolder.txtTo.setText(viewHolder.txtTo.getText().toString() + ":" + model.getTo());
+                viewHolder.txtRoute.setText(viewHolder.txtRoute.getText().toString() + ":" + model.getRoute());
+                viewHolder.txtTime.setText(viewHolder.txtTime.getText().toString() + ":" + model.getTime());
+
+            }
+        };
+        reportAdapter.notifyDataSetChanged();
+        reportRecyclerView.setLayoutManager(layoutManager);
+        reportRecyclerView.setAdapter(reportAdapter);
         return view;
     }
 }
